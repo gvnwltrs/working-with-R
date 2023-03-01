3e07d678b53b6d3431ae9e68dcd6235643bdd921
Gavin <waltersg298@gmail.com>
2023-02-28 19:26:10 -0700

    initial commit

---
+++ hello.R
@@ -0,0 +1,9 @@
+Hello <- function ( name, age ) {
+  sprintf("Hello, %s, how is your day going -- rumor has it you just turned %i years old!", name, age); 
+}
+
+numbers <- c(1,2,3)
+
+plot(numbers)
+plot(density(numbers))
+hist(numbers)
